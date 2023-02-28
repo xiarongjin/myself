119082fb7f82f0ce419023fe95db93103d60dbe2
xiarj <webxrj@gmail.com>
2023-02-28 15:54:44 +0800

    fix:

--- site/src/App.vue
+++ site/src/App.vue
@@ -2,6 +2,8 @@
   <div>{{ page }}</div>
   <el-button @click="addData">page++</el-button>
   <el-button @click="toExcel">导出</el-button>
+  <input type="text" v-model="title" />
+  <el-button @click="start">提交</el-button>
   <el-table :data="tableData" style="width: 100%">
     <el-table-column
       v-for="item in [
@@ -29,22 +31,24 @@ import * as XLSX from "xlsx";
 import axios from "axios";
 const tableData = ref([{ title: "test" }]);
 const page = ref(1);
-const getData = async (page) => {
+const title = ref();
+const getData = async (page, title) => {
   const res = await axios.post("http://localhost:3839/get", {
-    part: "shop-all-eyes",
+    part: title,
     page: page,
   });
   return res.data;
 };
 
-onMounted(async () => {
-  const data = await getData(page.value);
+onMounted(async () => {});
+const start = async () => {
+  const data = await getData(page.value, title.value);
   tableData.value = data;
-});
+};
 const addData = async () => {
   page.value += 1;
   try {
-    const data = await getData(page.value);
+    const data = await getData(page.value, title.value);
     tableData.value = tableData.value.concat(...data);
   } catch (error) {
     console.log(error);
@@ -56,8 +60,8 @@ const toExcel = () => {
   // 创建一个新的工作表
   const worksheet = XLSX.utils.json_to_sheet(tableData.value);
   // 将工作表附加到工作簿，并将工作表命名为students
-  XLSX.utils.book_append_sheet(workbook, worksheet, "shop-all-eyes");
+  XLSX.utils.book_append_sheet(workbook, worksheet, title.value);
   // 导出工作簿，并命名导出文件名为Presidents.xlsx
-  XLSX.writeFile(workbook, "shop-all-eyes.xlsx");
+  XLSX.writeFile(workbook, title.value + ".xlsx");
 };
 </script>
